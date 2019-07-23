f312667fb2824ac28960620795b301d7776c0faa
Ilona Brinkmeier <39468450+IloBe@users.noreply.github.com>
2019-07-23 23:06:58 +0200

    Fix: add 2 specific transfer model calls

--- models/Model.py
+++ models/Model.py
@@ -29,7 +29,10 @@ class Model:
         elif type_name in ["Improved"]:
             self.model_class = Improved_CNN_Model(name=name, metric=metric)
         elif type_name in ["Transfer"]:
-            self.model_class = Transfer_CNN_Model(name=name, metric=metric, train_inceptV3=train_inceptV3)
+            if self.name in ["Transfer_ResNet_CNN_Model"]:
+                self.model_class = Transfer_ResNet_CNN_Model(name=name, metric=metric)
+            elif self.name in ["Transfer_InceptionV3_CNN_Model"]:
+                self.model_class = Transfer_InceptionV3_CNN_Model(name=name, metric=metric)
         else:
             print("Wrong Model type - {} -, does not exist, therefore no model building possible.".format(type_name))
 
